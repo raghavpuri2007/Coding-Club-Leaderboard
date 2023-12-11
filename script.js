7126a191dcf7e8d8517f1785178a7e2776cb82d7
raghavpuri2007 <82910597+raghavpuri2007@users.noreply.github.com>
2023-12-10 18:35:07 -0800

    update 12/10

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Sample data for AP CS class
     const apcsUsers = [
-        { name: "Aadi Shah", points: 100, classPeriod: 6 },
-        { name: "Logan Lighthall", points: 100, classPeriod: 7 },
+        { name: "Aadi Shah", points: 300, classPeriod: 6 },
+        { name: "Aaron Read", points: 200, classPeriod: 6 },
+        { name: "Alex North", points: 300, classPeriod: 7 },
+        { name: "Alex Ko", points: 300, classPeriod: 6 },
+        { name: "Alex Huang", points: 100, classPeriod: 6 },
+        { name: "Amit Yosefi", points: 100, classPeriod: 4 },
+        { name: "Amy Zednick", points: 100, classPeriod: 5 },
+        { name: "Arunima Deval", points: 100, classPeriod: 7 },
         { name: "Calder Davis", points: 100, classPeriod: 7 },
-        { name: "Alex North", points: 100, classPeriod: 7 },
-        { name: "Thomas Dela Merced", points: 100, classPeriod: 4 },
-        { name: "Eshaan Chowdhry", points: 100, classPeriod: 5 },
+        { name: "Daniel Reinholdtsen", points: 100, classPeriod: 6 },
+        { name: "Daniel Major", points: 0, classPeriod: 5 },
+        { name: "Eshaan Chowdhry", points: 300, classPeriod: 5 },
+        { name: "Evenne Burgess", points: 100, classPeriod: 6 },
+        { name: "Ishan Awasthi", points: 200, classPeriod: 4 },
+        { name: "Jaylon Paulson", points: 200, classPeriod: 4 },
+        { name: "Jerusha Arun", points: 200, classPeriod: 4 },
+        { name: "Kara Ko", points: 300, classPeriod: 6 },
+        { name: "Kimiya Shafaat", points: 100, classPeriod: 4 },
+        { name: "Logan Lighthall", points: 100, classPeriod: 7 },
+        { name: "Lyle Whittlinger", points: 100, classPeriod: 7 },
+        { name: "Meri Galstyan", points: 300, classPeriod: 6 },
+        { name: "Nate Pickering", points: 100, classPeriod: 6 },
+        { name: "Sewit Deresse", points: 200, classPeriod: 4 },
         { name: "Shriya Patel", points: 100, classPeriod: 7 },
-        // { name: "James Wilson", points: 80, classPeriod: 1 },
-        // { name: "Sophia Miller", points: 75, classPeriod: 2 },
-        // { name: "Daniel Moore", points: 70, classPeriod: 1 },
-        // { name: "Ava Taylor", points: 65, classPeriod: 2 },
+        { name: "Thomas Dela Merced", points: 300, classPeriod: 4 },
+        { name: "Thomas Cooper", points: 100, classPeriod: 5 },
     ];
 
     // Sample data for Advanced Programming class
     const advancedProgrammingUsers = [
         { name: "Hiruy Derese", points: 100, classPeriod: 2 },
         { name: "Jishnu Mehta", points: 50, classPeriod: 1 },
-        // { name: "Ella Martinez", points: 130, classPeriod: 1 },
-        // { name: "William Garcia", points: 20, classPeriod: 2 },
-        // { name: "Grace Robinson", points: 110, classPeriod: 1 },
-        // { name: "Liam Davis", points: 100, classPeriod: 2 },
-        // { name: "Chloe Smith", points: 95, classPeriod: 1 },
-        // { name: "Mason Taylor", points: 90, classPeriod: 2 },
-        // { name: "Zoe Brown", points: 85, classPeriod: 1 },
-        // { name: "Ethan Wilson", points: 80, classPeriod: 2 },
     ];
 
     let currentLeaderboard = "APCS";
